f3a9f6af25ce16324c31073dd2b47af6ea8a1f94
Scott So <scotthhso@gmail.com>
2014-08-13 11:31:23 -0400

    implementing Jekyll with tutorial template

--- home.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-    $("#scroll").fadeIn(6000);
-    $("#sig").fadeIn(3000);
-  });
-
--- projects/100Days/day13/day13.js
+++
@@ -1,3 +0,0 @@
-setTimeout(function(){
-    document.getElementById('scare').style.display = 'block';
-},8000);
